2b5c197636cccc5a346b324990c705bc6710d4be
Ken Wilcox <wilcoxk@gmail.com>
2022-11-19 22:48:57 -0700

    Added README and dotnet project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ziglings Answers
+
+This repository contains my answers for [ziglings](https://github.com/ratfactor/ziglings)
+
+I tried to do the challenge on 063_labels, but I couldn't ever get my changes to compile. Maybe some other day.
+
+I used
+
+```bash
+% zig10 version
+0.10.0-dev.4060+61aaef0b0
+```
+
+On macOS 12.6 and 12.6.1
+
+The ziglings folder contains a .NET (dotnet) project that I hacked together to check in the examples once I completed the project. I was about 1/3 of the way through the exercises before I had the idea of creating a git repo for the answers and this allowed me to check them in based on the date/time I saved the files.
+
+Zig was fun and interesting. I'm not sure if I will ever use it.
+
+Good luck!
---
+++ ziglings/ziglings/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+const string moveFrom = @"/Users/ken/Developer/zig/ziglings/exercises";
+const string moveTo = @"/Users/ken/Developer/zig/ziglings-answers";
+
+Directory.SetCurrentDirectory(moveTo);
+foreach (var file in Directory.EnumerateFiles(moveFrom, "0*_*.zig").OrderBy(x => x))
+{
+    var fi = new FileInfo(file);
+    // if (fi.Name.StartsWith("031_")) break;
+    var newName = file.Replace(moveFrom, moveTo);
+    if (File.Exists(newName)) continue;
+    Console.WriteLine($"copying {file} to {newName}");
+    File.Copy(file, newName, true);
+    
+    var date = fi.LastWriteTime;
+    var dateStr = date.ToString("MMM dd HH:mm:ss yyyy");
+    var commitFile = Path.GetFileNameWithoutExtension(fi.Name);
+    commitFile = commitFile.Substring(commitFile.IndexOf('_')+1);
+    var commitMessage = $"add {commitFile}";
+    Console.WriteLine(dateStr);
+    // git add 001_hello.zig 
+    // git commit --date "Sep 14 19:25:34 2022" -m "add hello"
+    var p = Process.Start("git", $"add {fi.Name}");
+    p.WaitForExit();
+    p = Process.Start("git", $"commit --date \"{dateStr}\" -m \"{commitMessage}\"");
+    p.WaitForExit();
+    //Thread.Sleep(1);
+}
